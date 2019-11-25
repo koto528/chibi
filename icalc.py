c75fb2dda5c75c97ce38e0f61eafd09efec4eb8d
YAMAMOTO Kotoha <21716103@jwu.ac.jp>
2019-11-25 16:22:49 +0900

    icalc.py3

--- icalc.py
+++ icalc.py
@@ -7,10 +7,19 @@ Value = { [0-9]+ #Int}
 ''')
 parser = pegpy.generate(peg)
 
-t = parser('1+2')
-print(repr(t))
+def calc(t):
+    if t == 'Int':
+        return int(str(t))
+    if t == 'Add':
+        return calc(t[0]) + calc(t[1])
+    if t == 'Mul':
+        return calc(t[0]) * calc(t[1])
+    print(f'TODO {t.tag}')
+    return 0
 
-t = parser('1@2')
-print(repr(t))
+#t = parser('1+2*3+4*5')
+#print(repr(t))
+#print(calc(t))
+
 
 
