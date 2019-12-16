1c30949f9c9ce574432470863ff7304efc44e85a
YAMAMOTO Kotoha <21716103@jwu.ac.jp>
2019-12-16 16:40:46 +0900

    chibi6dayo

---
+++ chibi6.py
@@ -0,0 +1,8 @@
+class Binary(Expr):
+    __slots__ = ['left', 'right']
+    def __init__(self, left, right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
